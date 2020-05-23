783e5301d94714387aeb1d8aa79693db5af1d815
busraaozmen <busra.ozmenn@outlook.com>
2020-05-23 16:40:55 +0300

    Email Taghelper oluşturuldu. Sepetteki ürün sayısı sayıldı.

---
+++ LemonPieShop/Components/CategoryMenu.cs
@@ -0,0 +1,24 @@
+﻿using LemonPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LemonPieShop.Components
+{
+    public class CategoryMenu : ViewComponent
+    {
+        private readonly ICategoriesRepository _categoriesRepository;
+
+        public CategoryMenu(ICategoriesRepository categoriesRepository)
+        {
+           _categoriesRepository = categoriesRepository;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoriesRepository.AllCategories.OrderBy(c => c.CategoryName);
+            return View(categories);
+        }
+    }
+}
---
+++ LemonPieShop/Components/ShoppingCartSummary.cs
@@ -0,0 +1,34 @@
+﻿using BethanysPieShop.Models;
+using LemonPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LemonPieShop.Components
+{
+    public class ShoppingCartSummary: ViewComponent  //Görüntülüme bileşeni
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(shoppingCartViewModel);
+
+        }
+    }
+}
